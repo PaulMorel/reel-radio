1135e8724ea3222e2b67f94b311c1bab1830a891
Paul Morel <paul.morel@gmail.com>
2013-01-16 19:37:05 -0500

    More hierachy changes
    When using SimpLESS, compile the CSS file in the root of the projet
    folder, instead of compiling a the /assets/css folder. It's preparation
    for the eventual Wordpress migration

--- wp-content/reelradio-theme/header.php
+++ wp-content/reelradio-theme/header.php
@@ -1,19 +1,33 @@
+<?php
+/**
+ * Document header for the theme
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package reel_radio
+ * @since reel_radio 1.0
+ */
+?>
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html <?php language_attributes(); ?> class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"> <!--<![endif]-->
+<!--[if IEMobile 7 ]><html class="no-js iem7" manifest="default.appcache?v=1"><![endif]--> 
+<!--[if lt IE 7 ]><html class="no-js ie6" lang="en"><![endif]--> 
+<!--[if IE 7 ]><html class="no-js ie7" lang="en"><![endif]--> 
+<!--[if IE 8 ]><html class="no-js ie8" lang="en"><![endif]--> 
+<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!-->
+<html class="no-js" <?php language_attributes(); ?>><!--<![endif]-->
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width" />
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
 	<title><?php wp_title('&laquo;', true, 'right'); ?><?php bloginfo('name'); ?></title>
-	
 	<meta name="description" content="<?php bloginfo('description'); ?>">
 	<meta name="author" content="Paul Morel">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	
-	<link rel="shortcut icon" href="favicon.ico">
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+		
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 	
 	<script src="<?php bloginfo('template_url'); ?>/js/libs/modernizr-1.6.min.js"></script>
@@ -21,19 +35,23 @@
 	<?php wp_head() ?>
 	<?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
 	<!-- End Wordpress Head -->
-	
 </head>
 
-<body <?php body_class() ?>>
-<?php $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-	if ($lang == 'fr') { ?>
-		<div class="lang-warning"><p>Veuillez noter que la version française du site web est encore en dévelopment. <a href="http://paulmorel.com/2011/04/version-francaise/">Informations supplémentaires &raquo;</a></p></div>
-<?php } ?>
-<div class="wrapper">
-	<header>
-		<div class="logo ir"><a href="<?php echo get_option('home'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a></div>
-		<?php wp_nav_menu(array('container_class' => 'main-nav','theme_location' => 'primary')); ?>
-		<?php wp_nav_menu(array('container_class' => 'extra-nav-1','theme_location' => 'secondary','fallback_cb' => NULL)); ?>
-		<?php wp_nav_menu(array('container_class' => 'extra-nav-2','theme_location' => 'tertiary','fallback_cb' => NULL)); ?>
-	</header>
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+	<?php do_action( 'before' ); ?>
+	<header id="masthead" class="site-header" role="banner">
+		<hgroup>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+		</hgroup>
 
+		<nav role="navigation" class="site-navigation main-navigation">
+			<h1 class="assistive-text"><?php _e( 'Menu', '_s' ); ?></h1>
+			<div class="assistive-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', '_s' ); ?>"><?php _e( 'Skip to content', '_s' ); ?></a></div>
+
+			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+		</nav><!-- .site-navigation .main-navigation -->
+	</header><!-- #masthead .site-header -->
+
+	<div id="main" class="site-main">
