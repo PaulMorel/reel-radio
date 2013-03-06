dd354b7cb6a768afd72558022c840157d7a294de
Paul Morel <paul.morel@gmail.com>
2013-03-06 10:43:13 -0500

    Added post navigation
    Testing required, tranlsation required

--- wp-content/reelradio-theme/functions.php
+++ wp-content/reelradio-theme/functions.php
@@ -113,7 +113,51 @@ if ( function_exists('register_sidebars') )
         'before_title' => '<h3>',
         'after_title' => '</h3>'
     ));
-	
+
+function reel_content_nav( $nav_id ) {
+	global $wp_query, $post;
+
+	// Don't print empty markup on single pages if there's nowhere to navigate.
+	if ( is_single() ) {
+		$previous = ( is_attachment() ) ? get_post( $post->post_parent ) : get_adjacent_post( false, '', true );
+		$next = get_adjacent_post( false, '', false );
+
+		if ( ! $next && ! $previous )
+			return;
+	}
+
+	// Don't print empty markup in archives if there's only one page.
+	if ( $wp_query->max_num_pages < 2 && ( is_home() || is_archive() || is_search() ) )
+		return;
+
+	$nav_class = ( is_single() ) ? 'navigation-post' : 'navigation-paging';
+
+	?>
+	<nav role="navigation" id="<?php echo esc_attr( $nav_id ); ?>" class="<?php echo $nav_class; ?>">
+		<h1 class="assistive-text"><?php _e( 'Post navigation', '_s' ); ?></h1>
+
+	<?php if ( is_single() ) : // navigation links for single posts ?>
+
+		<?php previous_post_link( '<div class="previous">%link</div>', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', '_s' ) . '</span> %title' ); ?>
+		<?php next_post_link( '<div class="next">%link</div>', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', '_s' ) . '</span>' ); ?>
+
+	<?php elseif ( $wp_query->max_num_pages > 1 && ( is_home() || is_archive() || is_search() ) ) : // navigation links for home, archive, and search pages ?>
+
+		<?php if ( get_next_posts_link() ) : ?>
+		<div class="previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', '_s' ) ); ?></div>
+		<?php endif; ?>
+
+		<?php if ( get_previous_posts_link() ) : ?>
+		<div class="next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', '_s' ) ); ?></div>
+		<?php endif; ?>
+
+	<?php endif; ?>
+
+	</nav><!-- #<?php echo esc_html( $nav_id ); ?> -->
+	<?php
+}
+endif; // _s_content_nav
+
 /*function my_filter($posttypes)
 {
     // to remove posttypes of a particular name
