7dbc847e4dec8072cc905524e191810a1985c29d
Paul Morel <paul.morel@gmail.com>
2013-03-11 21:45:45 -0400

    Added 404 page to template
    Closes #8

---
+++ wp-content/reelradio-theme/404.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * 404 Error Page
+ *
+ * @package reel_radio
+ * @since reel_radio 1.0.1
+ */
+
+get_header(); ?>
+
+     <section class="main-content clearfix">
+     	<div class="wrapper">
+
+      <h1>Désolé, page non trouvée</h1>
+
+
+			<article>
+		      	<div class="entry-content">
+					<p>Cette ne semble pas exister. Pour signaler ce lien brisé cliquez <a href="mailto:prog@reel-radio.fm?subject=Lien Brisé">ici</a></p>
+					<p><a href="/">Revenir sur la page d'accueil</a></p>
+				</div>
+	     	</article>
+			
+<?php get_sidebar(); ?>	
+		</div>
+
+
+	</section><!-- section.site-content -->
+
+
+<?php get_footer(); ?>
