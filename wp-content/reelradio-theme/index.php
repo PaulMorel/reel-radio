56a579cdcdd2f76bdb0632b78474bbd63349a337
Paul Morel <paul.morel@gmail.com>
2013-03-22 15:39:33 -0400

    Updated front-page audio player styles

--- wp-content/reelradio-theme/index.php
+++ wp-content/reelradio-theme/index.php
@@ -19,10 +19,8 @@ get_header(); ?>
 			<!-- Listen -->
 				<article class="listen">
 		        <div class="article-content">
+		        	<p class="large-play"><i><a href="http://50.7.242.114:7561/listen.m3u" title="Écouter la radio!">P</a></i></p>
 			        <h2 class="entry-title">Écouter <span class="highlight">Réél-Radio</span></h2>
-			        <div class="excerpt">
-			        	<p class="large-play"><i><a href="http://50.7.242.114:7561/listen.m3u" title="Écouter la radio!">P</a></i></p>
-			        </div>
 		        </div>
 		        </article>
 			<?php /* Start the Loop */ ?>
